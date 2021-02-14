a4b4ebff66340116ad0a321eda0bb74a38796a44
Ernestas <simutisernestas@gmail.com>
2021-02-14 20:25:24 +0200

    few redundancies

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for YOLACT Edge."""
-
-__author__ = """Ernestas Šimutis"""
-__email__ = 'simutisernestas@gmail.com'
-__version__ = '0.1.0'
--- yolact_edge.py
+++
@@ -1,7 +0,0 @@
-"""Main module."""
-
-import yolact_edge.config
-
-class YOLACTEdge():
-  def __init__(self):
-    print("Init :)")
