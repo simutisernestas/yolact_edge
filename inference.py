fc7dbfd59925603a2a57cb3abd71c827ac7945b7
Ernestas <simutisernestas@gmail.com>
2021-02-15 21:13:25 +0200

    proper prediction output

--- inference.py
+++ inference.py
@@ -4,10 +4,19 @@ from pathlib import Path
 import torch
 import torch.backends.cudnn as cudnn
 import matplotlib.pyplot as plt
+from collections import defaultdict
+import cv2
 from yolact_edge.data.config import cfg, set_cfg
 from yolact_edge.yolact import Yolact
 from yolact_edge.utils.augmentations import FastBaseTransform
-from yolact_edge.eval import prep_display
+from yolact_edge.utils import timer
+from yolact_edge.layers.output_utils import postprocess, undo_image_transformation
+from yolact_edge.data import COLORS
+
+iou_thresholds = [x / 100 for x in range(50, 100, 5)]
+coco_cats = {}  # Call prep_coco_cats to fill this
+coco_cats_inv = {}
+color_cache = defaultdict(lambda: {})
 
 
 class SavePath:
@@ -197,6 +206,7 @@ class YOLACTEdgeInference(object):
         config = model_path.model_name + '_config'
         print('Parsed %s from the file name.\n' % config)
         set_cfg(config)
+        cfg.mask_proto_debug = False
 
         with torch.no_grad():
             if torch.cuda.is_available():
@@ -216,8 +226,123 @@ class YOLACTEdgeInference(object):
             net = net.cuda()
             self.net = net
 
+    def prep_output(self, dets_out, img, h, w, undo_transform=True, class_color=False, mask_alpha=0.45):
+        """
+        Note: If undo_transform=False then im_h and im_w are allowed to be None.
+        """
+        if undo_transform:
+            img_numpy = undo_image_transformation(img, w, h)
+            img_gpu = torch.Tensor(img_numpy).cuda()
+        else:
+            img_gpu = img / 255.0
+            h, w, _ = img.shape
+
+        with timer.env('Postprocess'):
+            t = postprocess(dets_out, w, h, visualize_lincomb=args.display_lincomb,
+                            crop_masks=args.crop,
+                            score_threshold=args.score_threshold)
+            torch.cuda.synchronize()
+
+        with timer.env('Copy'):
+            if cfg.eval_mask_branch:
+                # Masks are drawn on the GPU, so don't copy
+                masks = t[3][:args.top_k]
+            classes, scores, boxes = [
+                x[:args.top_k].cpu().detach().numpy() for x in t[:3]]
+
+        num_dets_to_consider = min(args.top_k, classes.shape[0])
+        for j in range(num_dets_to_consider):
+            if scores[j] < args.score_threshold:
+                num_dets_to_consider = j
+                break
+
+        if num_dets_to_consider == 0:
+            # No detections found so just output the original image
+            return (img_gpu * 255).byte().cpu().numpy()
+
+        # Quick and dirty lambda for selecting the color for a particular index
+        # Also keeps track of a per-gpu color cache for maximum speed
+        def get_color(j, on_gpu=None):
+            global color_cache
+            color_idx = (
+                classes[j] * 5 if class_color else j * 5) % len(COLORS)
+
+            if on_gpu is not None and color_idx in color_cache[on_gpu]:
+                return color_cache[on_gpu][color_idx]
+            else:
+                color = COLORS[color_idx]
+                if not undo_transform:
+                    # The image might come in as RGB or BRG, depending
+                    color = (color[2], color[1], color[0])
+                if on_gpu is not None:
+                    color = torch.Tensor(color).to(on_gpu).float() / 255.
+                    color_cache[on_gpu][color_idx] = color
+                return color
+
+        # First, draw the masks on the GPU where we can do it really fast
+        # Beware: very fast but possibly unintelligible mask-drawing code ahead
+        # I wish I had access to OpenGL or Vulkan but alas, I guess Pytorch tensor operations will have to suffice
+        if args.display_masks and cfg.eval_mask_branch:
+            # After this, mask is of size [num_dets, h, w, 1]
+            masks = masks[:num_dets_to_consider, :, :, None]
+
+            # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
+            colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(
+                1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
+            masks_color = masks.repeat(1, 1, 1, 3) * colors * mask_alpha
+
+            # This is 1 everywhere except for 1-mask_alpha where the mask is
+            inv_alph_masks = masks * (-mask_alpha) + 1
+
+            # I did the math for this on pen and paper. This whole block should be equivalent to:
+            #    for j in range(num_dets_to_consider):
+            #        img_gpu = img_gpu * inv_alph_masks[j] + masks_color[j]
+            masks_color_summand = masks_color[0]
+            if num_dets_to_consider > 1:
+                inv_alph_cumul = inv_alph_masks[:(
+                    num_dets_to_consider-1)].cumprod(dim=0)
+                masks_color_cumul = masks_color[1:] * inv_alph_cumul
+                masks_color_summand += masks_color_cumul.sum(dim=0)
+
+            img_gpu = img_gpu * \
+                inv_alph_masks.prod(dim=0) + masks_color_summand
+
+        # Then draw the stuff that needs to be done on the cpu
+        # Note, make sure this is a uint8 tensor or opencv will not anti alias text for whatever reason
+        img_numpy = (img_gpu * 255).byte().cpu().numpy()
+
+        if args.display_text or args.display_bboxes:
+            for j in reversed(range(num_dets_to_consider)):
+                x1, y1, x2, y2 = boxes[j, :]
+                color = get_color(j)
+                score = scores[j]
+
+                if args.display_bboxes:
+                    cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
+
+                if args.display_text:
+                    _class = cfg.dataset.class_names[classes[j]]
+                    text_str = '%s: %.2f' % (
+                        _class, score) if args.display_scores else _class
+
+                    font_face = cv2.FONT_HERSHEY_DUPLEX
+                    font_scale = 0.6
+                    font_thickness = 1
+
+                    text_w, text_h = cv2.getTextSize(
+                        text_str, font_face, font_scale, font_thickness)[0]
+
+                    text_pt = (x1, y1 - 3)
+                    text_color = [255, 255, 255]
+
+                    cv2.rectangle(img_numpy, (x1, y1),
+                                  (x1 + text_w, y1 - text_h - 4), color, -1)
+                    cv2.putText(img_numpy, text_str, text_pt, font_face,
+                                font_scale, text_color, font_thickness, cv2.LINE_AA)
+
+        return img_numpy, classes, scores, masks
+
     def predict(self, img, show=False):
-        # frame = torch.from_numpy(cv2.imread(path)).cuda().float()
         frame = torch.Tensor(img).cuda().float()
         batch = FastBaseTransform()(frame.unsqueeze(0))
 
@@ -226,9 +351,12 @@ class YOLACTEdgeInference(object):
 
         preds = self.net(batch, extras=extras)["pred_outs"]
 
+        img_numpy, classes, scores, masks = self.prep_output(
+            preds, frame, None, None, undo_transform=False)
+
         if show:
-            img_numpy = prep_display(
-                preds, frame, None, None, undo_transform=False)
             plt.imshow(img_numpy)
             plt.title("YOLACT Prediction! :)")
             plt.show()
+
+        return {"img": img_numpy, "class": classes, "socre": scores, "mask": masks.squeeze()}
